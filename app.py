4e729bee193524d2a1f4e5834ced3f33cd2fdda0
geschutaf-lang <geschutaf@gmail.com>
2026-06-02 05:10:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly.express as px
 st.set_page_config(page_title="HAA Quant Dashboard", layout="wide")
 
 CANARY    = ['TIP']
-OFFENSIVE = ['SPY', 'IWM', 'VEA', 'VWO', 'IEF', 'TLT', 'VNQ', 'PDBC', ]
-DEFENSIVE = ['IEF', 'BIL', 'GLD']
+OFFENSIVE = ['SPY', 'IWM', 'VEA', 'VWO', 'IEF', 'TLT', 'VNQ', 'PDBC',' QQQ' ]
+DEFENSIVE = ['IEF', 'BIL', 'GLD','PDBC']
 TOP_N     = 4
 MOM_PERIODS = [1, 3, 6, 12]
 
